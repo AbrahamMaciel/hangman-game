ada677a18a4e2bfe38086577818041edd0f6b0a7
Abraham Maciel <fernandezabmaciel@gmail.com>
2025-02-09 01:01:51 -0300

    Base Elements done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,60 @@
 import { useState } from "react";
 import styles from "./App.module.css";
+import { languages } from "./languages.js";
 
 function App() {
+  const [currentWord, setCurrentWord] = useState("react");
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  const languageElements = languages.map((langObj) => {
+    const chipStyles = {
+      backgroundColor: langObj.backgroundColor,
+      color: langObj.color,
+    };
+    return (
+      <span style={chipStyles} className={styles.chip} key={langObj.name}>
+        {langObj.name}
+      </span>
+    );
+  });
+
+  const lettersElements = currentWord.split("").map((letter, index) => (
+    <span key={index} className={styles.letter}>
+      {letter.toUpperCase()}
+    </span>
+  ));
+
+  const keyboardElements = alphabet.split("").map((letter) => (
+    <button className={styles.key} key={letter}>
+      {letter.toUpperCase()}
+    </button>
+  ));
+
   return (
     <>
-      <div className={styles.instructions}>
+      <header className={styles.instructions}>
         <h3>Assembly: Endgame</h3>
         <p className={styles.secondary}>
           Guess the word in under 8 attempts to keep the programming world safe
           from Assembly!
         </p>
-      </div>
-      <div>
-        <p>“Yay lots of choice, the world is safe”</p>
-      </div>
+        
+        <div className={`${styles.statusMessage} ${styles.victory}`}>
+          <h2>You Win!</h2>
+          <p>Well done! 🎉</p>
+        </div>
+      </header>
+
+      <section className={styles.languageChips}>{languageElements}</section>
+
+      <section className={styles.word}>{lettersElements}</section>
+
+      <section className={styles.keyboard}>{keyboardElements}</section>
+
+      <button type="button" className={styles.newGameBtn}>
+        New Game
+      </button>
     </>
   );
 }
